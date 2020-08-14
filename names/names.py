363757fb1ee7fe20a333051e87ff628a767fa8cb
Francisco Barrios <shighetari@gmail.com>
2020-08-14 12:37:45 -0400

    init commit, ring_buffer & names done.

--- names/names.py
+++ names/names.py
@@ -13,10 +13,58 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+''' ~~~~ IMPLEMENTING A BST Optimizing some inefficient code ''' 
+# Creating a BST DS
+class BSTNode:
+    def __init__(self, value):
+        self.value = value 
+        self.left = None
+        self.right = None
+
+    # BST INSERT METHOD
+    # Insert the given value into the tree
+    def insert(self, value):
+        # check if the value is less than the current node's value
+        if value < self.value:
+            # does the current node have a left child?
+            if self.left:
+                self.left.insert(value)
+                # otherwise, it doesn't have a left child and we can park the new node here
+            else:
+                self.left = BSTNode(value)
+        # otherwise the value is greater or equal to the current node's value
+        else:
+            # does the current node have a right child?
+            if self.right:
+                # if it does, call the right child's `insert` method to repeat the process
+                self.right.insert(value)
+            #otherwise, it doesn't have a right child, we can park the new node here
+            else:
+                self.right = BSTNode(value)
+    # CONTAIN CHECK IF BST HAS THE STORED VALUE            
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target <= self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target) # recursion loop left
+        else: 
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target) # recursion loop right
+
+bst = BSTNode("")
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    bst.insert(name_1)
+for name_2 in names_2:
+    if bst.contains(name_2):
+        duplicates.append(name_2)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
 class RingBuffer:
+    
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.stack = []
+        self.free_index = 0
 
     def append(self, item):
-        pass
-
+        if len(self.stack) < self.capacity:
+            #insert item @ free index
+            self.stack.append(item)
+        elif len(self.stack) == self.capacity:
+            if self.free_index == self.capacity:
+                self.free_index = 0
+            self.stack.pop(self.free_index)
+            self.stack.insert(self.free_index, item)
+            self.free_index = self.free_index + 1
     def get(self):
-        pass
+        return self.stack
+# testing visually for my sake
+# buffer = RingBuffer(3)
+# print(buffer.append(1))
+# print(buffer.append(2))
+# print(buffer.append(3))
+# print(buffer.append(4))
+# print(buffer.append(5))
+# print(buffer.append(6))
